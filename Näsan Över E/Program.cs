4faab6acc8d6d553fa04464a528c4382e0d1c85c
anton svedin <anton.svedin@elev.ga.ntig.se>
2024-11-11 09:38:10 +0100

    Har gjort dom första 3 övningarna

---
+++ Näsan Över E/Program.cs
@@ -0,0 +1,43 @@
+﻿
+// Övning 1 
+// if(6 >= 3)
+// {
+//     Console.WriteLine("Hello, World!");
+//     Console.ReadLine();
+// }
+
+// Övning 2a
+// Console.WriteLine("Skriv in ditt användarnamn");
+// string användarnamn = Console.ReadLine();
+
+// if(användarnamn == "kalleanka")
+// {
+//    Console.WriteLine("Welcome!");
+//    Console.ReadLine(); 
+// }
+
+// Övning 2b
+// Console.WriteLine("Skriv in ditt användarnamn");
+// string användarnamn = Console.ReadLine();
+
+// Console.WriteLine("Skriv in ditt lösenord");
+// string lösenord = Console.ReadLine();
+
+// if(användarnamn == "kalleanka" && lösenord == "12345")
+// {
+//     Console.WriteLine("Welcome!"); 
+//     Console.ReadLine();
+// }
+// else
+// {
+//     Console.WriteLine("Wrong username or password");
+//     Console.ReadLine();
+// }
+
+// Övning 3
+// int i;
+// for (i = 1; i <= 32; i++)
+// {
+//     Console.WriteLine("Hello, World!");
+// }
+// Console.ReadLine();
