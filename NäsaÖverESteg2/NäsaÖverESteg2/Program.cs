07fa8b0f3c1d75062d8cf5881e49c17a8cc9a52d
anton svedin <anton.svedin@elev.ga.ntig.se>
2024-11-22 10:25:45 +0100

    Gjort dom 4 första punkterna

---
+++ NäsaÖverESteg2/NäsaÖverESteg2/Program.cs
@@ -0,0 +1,23 @@
+﻿// List<string> Leksaker = [
+// "Teddybjörn",
+// "Studsboll",
+// "Hopprep",
+// "Trampbil",
+// "Lådbil",
+// ];
+
+// for(int i = 0; i < Leksaker.Count; i++)
+// {
+//     Console.WriteLine($"[{i}]  {Leksaker[i]}");
+// }
+// Console.ReadLine();
+
+// List<string> Klasskamrater = [
+// "Joakim",
+// "Dean",
+// "Martin",
+// "Rasmus",
+// "Felix",
+// ];
+
+// List<int>intLista = new() {2,5,9,1,6};
